b019454aefb8c078f9e7623b4bed1cf606fe3b51
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-03-08 11:39:58 -0500

    project cleaned

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -10,17 +10,6 @@ export const SMURF_UPDATE_SUCCESS = 'SMURFS_UPDATE_SUCCESS';
 export const SMURF_DELETE = 'SMURFS_DELETE';
 export const SMURF_DELETE_SUCCESS = 'SMURFS_DELETE_SUCCESS';
 
-/*
-  For this project you'll need at least 2 action creators for the main portion,
-   and 2 more for the stretch problem.
-   Be sure to include action types for each type of action creator. Also, be sure to mind
-     the "pending" states like, fetching, creating, updating and deleting.
-   C - addSmurf
-   R - getSmurfs
-   U - updateSmurf
-   D - deleteSmurf
-*/
-
 export const getSmurfs = () => {
   return dispatch => {
     dispatch({ type: SMURFS_FETCH })
