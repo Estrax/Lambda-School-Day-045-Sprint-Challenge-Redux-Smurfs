313adb40565ebf5283404509df115e75d5bbd31b
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-03-08 11:46:45 -0500

    added redux to app component

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-/*
- to wire this component up you're going to need a few things.
- I'll let you do this part on your own. 
- Just remember, `how do I `connect` my components to redux?`
- `How do I ensure that my component links the state to props?`
- */
+import { connect } from 'react-redux';
+import { getSmurfs } from '../actions';
+
 class App extends Component {
+  componentDidMount() {
+    this.props.getSmurfs();
+  }
   render() {
+    console.log(this.props.smurfs);
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
@@ -19,4 +20,23 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    smurfsFetching: state.smurfsFetching,
+    smurfsFetched: state.smurfsFetched,
+    smurfAdd: state.smurfAdd,
+    smurfAdded: state.smurfAdded,
+    smurfUpdate: state.smurfUpdate,
+    smurfUpdated: state.smurfUpdated,
+    smurfDelete: state.smurfDelete,
+    smurfDeleted: state.smurfDeleted,
+    error: state.error,
+    smurfs: state.smurfs
+  }
+}
+
+const mapDispatchToProps = {
+  getSmurfs
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
