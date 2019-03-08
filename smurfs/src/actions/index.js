09177faa578db48c3713d81a80c214e68ecdb17a
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-03-08 11:23:18 -0500

    fixed typo in action types

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -3,10 +3,10 @@ export const SMURFS_FETCH = 'SMURFS_FETCH';
 export const SMURFS_FETCH_SUCCESS = 'SMURFS_FETCH_SUCCESS';
 export const SMURF_ADD = 'SMURF_ADD';
 export const SMURF_ADD_SUCCESS = 'SMURF_ADD_SUCCESS';
-export const SMURFS_UPDATE = 'SMURFS_UPDATE';
-export const SMURFS_UPDATE_SUCCESS = 'SMURFS_UPDATE_SUCCESS';
-export const SMURFS_DELETE = 'SMURFS_DELETE';
-export const SMURFS_DELETE_SUCCESS = 'SMURFS_DELETE_SUCCESS';
+export const SMURF_UPDATE = 'SMURFS_UPDATE';
+export const SMURF_UPDATE_SUCCESS = 'SMURFS_UPDATE_SUCCESS';
+export const SMURF_DELETE = 'SMURFS_DELETE';
+export const SMURF_DELETE_SUCCESS = 'SMURFS_DELETE_SUCCESS';
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
